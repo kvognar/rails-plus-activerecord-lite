65d06cedd289b1721637e7423ae5118af803d652
Kevin Vognar <kvognar@gmail.com>
2014-09-03 15:07:49 -0700

    Started compiling Rails Lite

---
+++ controller_base.rb
@@ -0,0 +1,40 @@
+class ControllerBase
+  
+  def initialize(req, res, route_params = {})
+    
+  end
+  
+  def already_built_response?
+    @already_built_response ||= false
+  end
+  
+  def redirect_to(url)
+    raise "Cannot render or redirect more than once" if already_built_response?
+    @res.status = 302
+    @res.header['location'] = url
+    session.store_session(@res)
+    @already_built_response = true
+  end
+  
+  def render_content(content, type)
+    raise "Cannot render or redirect more than once" if already_built_response?
+    @res.content_type = type
+    @res.body = content
+    session.store_session(@res)
+    @already_built_response = true
+  end
+  
+  def render(template_name)
+    file = File.read(
+      "views/#{self.class.name.underscore}/#{template_name}.html.erb")
+    view = ERB.new(file).result(binding)
+    render_content(view, "text/html")
+  end
+  
+  def session
+    @session ||= Session.new(@req)
+  end
+  
+  
+  
+end
